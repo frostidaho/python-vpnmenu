fd3df9829fd99402d8dc0a1b674ac64cbd6b0057
Idaho Frost <frostidaho@gmail.com>
2016-10-23 17:15:43 -0400

    Add python-networkmanager as dependency

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
+        'python-networkmanager',
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
